4a19fa68dd6963879ac152fd51cd6e6ba280d34c
中村俊介 <nakamura@feliseed.co.jp>
2022-09-16 15:32:07 +0900

    refactoring

--- Plane.js
+++ Plane.js
@@ -29,13 +29,57 @@ class Plane {
         const secondTriangle = new Triangle(initVertex, secondVertex, fourthVertex, initEdge, fourthEdge, fifthEdge);
         this.triangles.add(initTriangle);
         this.triangles.add(secondTriangle);
-        const result = initVertex.getFourVertexesBy(this);
+    }
+
+    getObjBy(vertex) {
+        const resultVertexes = new Set();
+        const resultEdges = new Set();
+        const resultRectangles = new Set();
+        const edgesNotSandwiched = vertex.getConnectedAllEdgesNotSandwichedBy(this);
+        edgesNotSandwiched.forEach((edge) => {
+            const vertexes = edge.getVertexes();
+            const tmpV = new Set();
+            const tmpE = new Set([edge]);
+            vertexes.forEach((vertex) => {
+                const adjacentingPolygon = edge.getAdjacentingAllPolygonsBy(this)[Symbol.iterator]().next().value;
+                const newVertex = Vertex.getRectangleVertexOppositeSideOf(vertex, edge, adjacentingPolygon);
+                resultVertexes.add(newVertex);
+                tmpV.add(newVertex);
+                tmpE.add(new Edge(vertex, newVertex));
+                resultEdges.add(new Edge(vertex, newVertex));
+            });
+            resultEdges.add(new Edge(Array.from(tmpV)[0], Array.from(tmpV)[1]))
+            resultRectangles.add(new Rectangle(
+                edge.v1,
+                Array.from(tmpV)[0],
+                Array.from(tmpV)[1],
+                edge.v2,
+                edge,
+                Array.from(tmpE)[1],
+                new Edge(Array.from(tmpV)[0], Array.from(tmpV)[1]),
+                Array.from(tmpE)[2],
+            ));
+        });
+        return new Object({"resultVertexes": resultVertexes, "resultEdges": resultEdges, "resultRectangles":resultRectangles});
+    }
+
+    pasteTileAround(vertex) {
+        if(vertex.getDegreeBy(this) == 3 && vertex.getAroundPolygonNumBy(this) == 2) {
+            return this.getObjBy(vertex);
+        } else if(vertex.getDegreeBy(this) == 5 && vertex.getAroundPolygonNumBy(this) == 4) {
+
+        } else if(vertex.getDegreeBy(this) == 4 && vertex.getAroundPolygonNumBy(this) == 3) {
+
+        }
+    }
+
+    evo(vertex) {
+        const result = this.pasteTileAround(vertex);
+        console.log(result);
         this.vertexs.union(result.resultVertexes);
         this.edges.union(result.resultEdges);
         this.rectangles.union(result.resultRectangles);
-        console.table(this);
     }
-
 
     draw() {
         this.triangles.forEach((triangle) => {
--- Vertex.js
+++ Vertex.js
@@ -78,42 +78,6 @@ class Vertex {
             throw new Error('vertex is not included in this edge');
         }
     }
-    
-    getFourVertexesBy(plane) {
-        if(this.getDegreeBy(plane) == 3 && 
-            this.getAroundPolygonNumBy(plane) == 2
-        ){
-            const resultVertexes = new Set();
-            const resultEdges = new Set();
-            const resultRectangles = new Set();
-            const edgesNotSandwiched = this.getConnectedAllEdgesNotSandwichedBy(plane);
-            edgesNotSandwiched.forEach((edge) => {
-                const vertexes = edge.getVertexes();
-                const tmpV = new Set();
-                const tmpE = new Set([edge]);
-                vertexes.forEach((vertex) => {
-                    const adjacentingPolygon = edge.getAdjacentingAllPolygonsBy(plane)[Symbol.iterator]().next().value;
-                    const newVertex = Vertex.getRectangleVertexOppositeSideOf(vertex, edge, adjacentingPolygon);
-                    resultVertexes.add(newVertex);
-                    tmpV.add(newVertex);
-                    tmpE.add(new Edge(vertex, newVertex));
-                    resultEdges.add(new Edge(vertex, newVertex));
-                });
-                resultEdges.add(new Edge(Array.from(tmpV)[0], Array.from(tmpV)[1]))
-                resultRectangles.add(new Rectangle(
-                    edge.v1,
-                    Array.from(tmpV)[0],
-                    Array.from(tmpV)[1],
-                    edge.v2,
-                    edge,
-                    Array.from(tmpE)[1],
-                    new Edge(Array.from(tmpV)[0], Array.from(tmpV)[1]),
-                    Array.from(tmpE)[2],
-                ));
-            });
-            return new Object({"resultVertexes": resultVertexes, "resultEdges": resultEdges, "resultRectangles":resultRectangles});
-        }
-    }
 
     static getRectangleVertexOppositeSideOf(vertex, edge, triangle) {
         const ohterVertex = vertex.getOhterVertexOf(edge);
